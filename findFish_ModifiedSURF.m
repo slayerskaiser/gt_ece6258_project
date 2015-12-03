ecdfee708f450464b8afb7193ba8810133f7e028
Klaus <derkoenig@gmail.com>
2015-12-02 20:47:38 -0500

    Add fish detection results

--- findFish_ModifiedSURF.m
+++ findFish_ModifiedSURF.m
@@ -1,18 +1,18 @@
-function [ptsOld,ptsCur] = findMatch_ModifiedSURF(oldFrame,curFrame)
+function [ptsFrame,ptsFish] = findFish_ModifiedSURF(frame,frameMask,fish,fishMask)
 
-detector = cv.FeatureDetector('FastFeatureDetector');
-% detector = cv.FeatureDetector('SIFT');
+% detector = cv.FeatureDetector('FastFeatureDetector');
+detector = cv.FeatureDetector('SIFT');
 % detector = cv.FeatureDetector('SURF');
-ptsOld = detector.detect(oldFrame);
-ptsCur = detector.detect(curFrame);
+ptsFish = detector.detect(fish,'Mask',fishMask);
+ptsFrame = detector.detect(frame,'Mask',frameMask);
 detector.delete
 clear detector
-ptsOld = reshape([ptsOld.pt]+1,2,[]).';
-ptsCur = reshape([ptsCur.pt]+1,2,[]).';
+ptsFrame = reshape([ptsFrame.pt]+1,2,[]).';
+ptsFish = reshape([ptsFish.pt]+1,2,[]).';
 
-[featOld,ptsOld] = extractFeatures(oldFrame,ptsOld,'Method','SURF');
-[featCur,ptsCur] = extractFeatures(curFrame,ptsCur,'Method','SURF');
-indexPairs = matchFeatures(featOld, featCur, 'Prenormalized', true, ...
-    'Method', 'NearestNeighborRatio');
-ptsOld = ptsOld(indexPairs(:,1),:);
-ptsCur = ptsCur(indexPairs(:,2),:);
+[featFrame,ptsFrame] = extractFeatures(frame,ptsFrame,'Method','Block');
+[featFish,ptsFish] = extractFeatures(fish,ptsFish,'Method','Block');
+indexPairs = matchFeatures(featFrame, featFish, 'Prenormalized', false, ...
+    'Method', 'NearestNeighborSymmetric');
+ptsFrame = ptsFrame(indexPairs(:,1),:);
+ptsFish = ptsFish(indexPairs(:,2),:);
